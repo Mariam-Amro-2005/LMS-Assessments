aebb03d1cf92533cdf7b4c79a585986335571ff1
Mariam Amro <mariam.seifeldin.2005@gmail.com>
2024-12-18 03:07:34 +0200

    Made code work!

--- src/main/java/com/example/lms/assessments/repository/AssignmentSubmissionRepository.java
+++ src/main/java/com/example/lms/assessments/repository/AssignmentSubmissionRepository.java
@@ -2,9 +2,14 @@ package com.example.lms.assessments.repository;
 
 import com.example.lms.assessments.model.AssignmentSubmission;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface AssignmentSubmissionRepository extends JpaRepository<AssignmentSubmission, Integer> {
-    Optional<AssignmentSubmission> findByAssignment_AssignmentIdAndUserId(Integer assignmentId, Integer userId);
+
+    @Query("SELECT a FROM AssignmentSubmission a WHERE a.assignment.assessmentId = :assignmentId AND a.userId = :userId")
+    Optional<AssignmentSubmission> findByAssignment_AssignmentIdAndUserId(@Param("assignmentId") Integer assignmentId,
+                                                                          @Param("userId") Integer userId);
 }
--- src/main/java/com/example/lms/assessments/repository/QuestionRepository.java
+++ src/main/java/com/example/lms/assessments/repository/QuestionRepository.java
@@ -2,10 +2,13 @@ package com.example.lms.assessments.repository;
 
 import com.example.lms.assessments.model.Question;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface QuestionRepository extends JpaRepository<Question, Integer> {
 
-    List<Question> findByQuiz_QuizId(Integer quizId);
+    @Query("SELECT q FROM Question q JOIN q.quizzes quiz WHERE quiz.assessmentId = :quizId")
+    List<Question> findByQuizId(@Param("quizId") Integer quizId);
 }
